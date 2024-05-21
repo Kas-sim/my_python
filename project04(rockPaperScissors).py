e53848114caf5b83d0dfb934783c76eb4371ce84
Kas-sim <amkassim444@gmail.com>
2024-05-21 05:22:54 -0700

    finished version 1.0

--- project04(rockPaperScissors).py
+++ project04(rockPaperScissors).py
@@ -11,20 +11,42 @@ import random
 #computer = random.choice([r, p, s])
 
 moves = ["Rock", "Paper", "Scissor"]
-computer = random.choice(moves)
-user_input = (input("Choose between Rock Paper Scissor \n")).capitalize()
-print(computer)
+victory = 0
+lost = 0
+tie = 0
 
-if user_input not in moves:
-    quit("Try Again \n")
+while True:
+    user_input = (input("Type: Rock/Paper/Scissor or Q to quit\n")).capitalize()
+    computer = random.choice(moves)
 
-if user_input == "Rock" and computer == "Scissor":
-    print("You Won!")
-elif user_input == "Paper" and computer == "Rock":
-    print("You Won!")
-elif user_input == "Scissor" and computer == "Paper":
-    print("You Won!")
-elif user_input == computer:
-    print("Tie")
-else:
-    print("Computer Won!")
+    if user_input == "Q":
+        print()
+        print(f"You Won {victory} times!")  
+        print(f"You Lost {lost} times!")
+        print(f"Tie {tie} times!")
+        quit()
+
+    print("")
+    print(computer, "\n")
+    
+    if user_input not in moves:
+        quit("Invalid input! \n")
+
+    if user_input == "Rock" and computer == "Scissor":
+        print("You Won! \n")
+        victory += 1
+    elif user_input == "Paper" and computer == "Rock":
+        print("You Won! \n")
+        victory += 1
+    elif user_input == "Scissor" and computer == "Paper":
+        print("You Won! \n")
+        victory += 1
+    elif user_input == computer:
+        print("Tie \n")
+        tie += 1
+    else:
+        print("Computer Won! \n")
+        lost += 1
+    #print(f"You Won {victory} times and Lost {lost} times \n")
+    
+    
