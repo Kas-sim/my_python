32791d79b4333a668d44713b495ee8a056773ebe
Kas-sim <amkassim444@gmail.com>
2025-05-03 11:11:21 -0700

    AgeCalculator - 3/4/2025

---
+++ Learning/hello.py
@@ -0,0 +1,12 @@
+print("Hello Python")
+
+name = input("Enter your name: ")
+# print("Welcome back, " + name + "!")
+
+age = int(input("Enter your age: "))
+print(f"\nNice to meet you {name}!\nSo, you are {age} years old! ")
+
+added = int(input("After how many years? "))
+age += added
+print(f"In coming {added} years, you will be {age} years old")
+
---
+++ Python_projects/project05(ageCalculator).py
@@ -0,0 +1,36 @@
+print("\nAge Calculator - Know How many days you have lived!\n")
+
+name = input("Enter your Name: ")
+birthDate = int(input("Enter Birth Date: "))
+birthMonth = int(input("Enter Birth Month: "))
+birthYear = int(input("Enter Birth Year: "))
+
+print("\nEnter D/M/Y from which you want to calculate {name}'s Age: \n")
+fromDate = int(input("Date: "))
+fromMonth = int(input("Month: "))
+fromYear = int(input("Year: "))
+
+year = fromYear - birthYear
+
+# Month
+if birthMonth > fromMonth:
+    month = (12-birthMonth) + fromMonth
+    year-=1
+elif birthMonth < fromMonth:
+    month = fromMonth - birthMonth
+else :
+    month = 0
+
+# Date
+if birthDate > fromDate:
+    day = (30-birthDate) + fromDate
+    month-=1
+elif birthDate < fromDate:
+    day = fromDate - birthDate
+else :
+    day = 0
+
+daysLived = (365*year) + (30*month) + day
+
+print(f"{name} have lived {year} years, {month} months and {day} days")
+print(f"{name} have lived {daysLived} days")
