59d27511d2e9838b06526eab7e22bb8e5d4d20a1
Kas-sim <amkassim444@gmail.com>
2025-11-04 01:08:42 +0500

    updated to most recent verion of project

--- Python_projects/project05(ageCalculator).py
+++ Python_projects/project05(ageCalculator).py
@@ -5,37 +5,37 @@ while True:
     if check == "0" or "n" or "N":
         break
 
-    name = input("Enter your Name: ")
-    birthDate = int(input("Enter Birth Date: "))
-    birthMonth = int(input("Enter Birth Month: "))
-    birthYear = int(input("Enter Birth Year: "))
+name = input("Enter your Name: ")
+birthDate = int(input("Enter Birth Date: "))
+birthMonth = int(input("Enter Birth Month: "))
+birthYear = int(input("Enter Birth Year: "))
 
-    print("\nEnter D/M/Y from which you want to calculate {name}'s Age: \n")
-    fromDate = int(input("Date: "))
-    fromMonth = int(input("Month: "))
-    fromYear = int(input("Year: "))
+print("\nEnter D/M/Y from which you want to calculate {name}'s Age: \n")
+fromDate = int(input("Date: "))
+fromMonth = int(input("Month: "))
+fromYear = int(input("Year: "))
 
-    year = fromYear - birthYear
+year = fromYear - birthYear
 
-    # Month
-    if birthMonth > fromMonth:
-        month = (12-birthMonth) + fromMonth
-        year-=1
-    elif birthMonth < fromMonth:
-        month = fromMonth - birthMonth
-    else :
-        month = 0
+# Month
+if birthMonth > fromMonth:
+    month = (12-birthMonth) + fromMonth
+    year-=1
+elif birthMonth < fromMonth:
+    month = fromMonth - birthMonth
+else :
+    month = 0
 
-    # Date
-    if birthDate > fromDate:
-        day = (30-birthDate) + fromDate
-        month-=1
-    elif birthDate < fromDate:
-        day = fromDate - birthDate
-    else :
-        day = 0
+# Date
+if birthDate > fromDate:
+    day = (30-birthDate) + fromDate
+    month-=1
+elif birthDate < fromDate:
+    day = fromDate - birthDate
+else :
+    day = 0
 
-    daysLived = (365*year) + (30*month) + day
+daysLived = (365*year) + (30*month) + day
 
-    print(f"{name} have lived {year} years, {month} months and {day} days")
-    print(f"{name} have lived {daysLived} days\n")
+print(f"{name} have lived {year} years, {month} months and {day} days")
+print(f"{name} have lived {daysLived} days\n")
