8ce2fddeed51ea40f1b750f7b57ccaf98bbc8b6e
Kas-sim <amkassim444@gmail.com>
2025-11-18 01:50:25 +0500

    update successful - lol, gave some hard times

--- basics/employee.py
+++ basics/employee.py
@@ -2,9 +2,15 @@ import json
 filepath = "employee.json"
 
 def loadEmployees():
-    with open(filepath, "r") as f:
-        empData = json.load(f)
+    with open(filepath, "r") as file:
+        empData = json.load(file)
     return empData
+
+def saveEmployees():
+    empData = loadEmployees()
+    with open("updEmployee.json", "w") as file:
+        file.write(json.dumps(empData, indent=2))
+
 
 def printEmployees():
     empData = loadEmployees()
@@ -24,11 +30,38 @@ def searchEmployee():
     print(f"Employee with {inp} not found!")
 
 def updateEmployee():
+    empData = loadEmployees()
+    inp = int(input("\nEnter employee you want to update: "))
+    
+    for x in empData:
+        if inp == x["id"]:
+            print(f"Employee found!")
+            while True: 
+                inp = int(input("\n0. Exit \n1. Update Employee Name \n2. Update Employee Role \n3. Update Employee Salary \n\nEnter Operation: "))
+                
+                if inp == 0:
+                    break
+                elif inp == 1:
+                    name = input(f"Enter new name for Employee {x["id"]}: ")
+                    x["name"] = name
+                elif inp == 2:
+                    role = input(f"Enter new role for Employee {x["id"]}: ")
+                    x["role"] = role
+                elif inp == 3:
+                    salary = input(f"Enter new salary for Employee {x["id"]}: ")
+                    x["salary"] = salary
+                else:
+                    continue
 
+                with open (filepath, "w") as file:
+                    file.write(json.dumps(empData, indent=2))     
+                
+                return
+    print(f"Employee not found!")
 
 
 while True:
-    x = int(input("\n0. Exit \n1. Search for Employee \n2. Add Employee \n3. Update Employee \n4. Print Employees \n\nEnter operation: "))
+    x = int(input("\n0. Exit \n1. Search for Employee \n2. Update Employee  \n3. Add Employee \n4. Print Employees \n\nEnter operation: "))
 
     if x == 0:
         break
@@ -58,7 +91,9 @@ empData is list of json objects
 
 update Employee
     - load data
+    - ask the user which part he wants to modify
     - modify specific chunck
-    - re-write into 
+    - re-write back the updated 
+
 
 '''
