d0dd86272348c2ef0c3103ed66a37f092d0db519
Kas-sim <amkassim444@gmail.com>
2025-11-17 15:15:43 +0500

    fine working version | (with a littel security flaw)

--- basics/hello.py
+++ basics/hello.py
@@ -1,5 +1,5 @@
-def createAccount():
-    username = input("Enter username: ")
+def hashedInput():
+    username = input("Enter Username: ")
     password = input("Enter Password: ")
     
     hashedPass = ""
@@ -8,36 +8,38 @@ def createAccount():
         y = ord(x)
         z = chr(y+10)
         hashedPass += z
+    return username, hashedPass
+
+def createAccount():
+    print("\n--- Signup ---")
+    username, hashedPass = hashedInput()
 
     with open("accountData.txt", "r") as file:
         content = file.read()
-        if hashedPass in content:
-            print("this password already exists")
-            return
+        if username in content:
+            print(f"{username} is already registered!\n")
+            createAccount()
         else:
             with open("accountData.txt", "a") as file:
+                print(f"Congratulations, {username}\nAccount created successfully!\n")
                 file.write(f"{username} - {hashedPass}\n")
 
 def login():
-    usernme = input("Enter username: ")
-    password = input("Enter Password: ")
-
-    hashedPass = ""
-
-    for x in password:
-        y = ord(x)
-        z = chr(y+10)
-        hashedPass += z
+    print("\n--- Login ---")
+    username, hashedPass = hashedInput()
 
     with open("accountData.txt", "r") as file:
         content = file.read()
-        if hashedPass in content:
-            print("Login Successful!")
-
+        if username and hashedPass in content:
+            print("Login Successful!\n")
+            return
+        else:
+            print("Login Failed!\n")
+            return
 
 
 while True:
-    x = input("Enter operatin you want to perform (0 to quit): ")    
+    x = input("\n0. Exit \n1. Create Account \n2. Login \nEnter operation: ")    
     if x == "1":
         createAccount()
     elif x == "2":
@@ -46,3 +48,18 @@ while True:
         break
     else:
         continue
+
+'''
+Menu with (exit, signup, login)
+
+signup 
+    create account
+    only if user is not registered already!
+    writing in accountData.txt
+
+login
+    check username and password exist 
+    login failed or login successful
+    checking from accountData.txt
+
+'''
