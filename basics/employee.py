8672f4fa72d901079796936ca399bdc307b3bbc5
Kas-sim <amkassim444@gmail.com>
2025-11-18 00:53:10 +0500

    reading and searching done - time to update the json

--- basics/employee.py
+++ basics/employee.py
@@ -1,12 +1,64 @@
 import json
 filepath = "employee.json"
 
-with open(filepath, "r") as f:
-    empData = json.load(f)
-    # print(empData)
+def loadEmployees():
+    with open(filepath, "r") as f:
+        empData = json.load(f)
+    return empData
 
-# print( empData[0][""] )
+def printEmployees():
+    empData = loadEmployees()
+    print("\n")
+    for x in empData:
+        print(f"{x["id"]}. {x["name"]} -> {x["role"]}")
+    print("\n")
 
-for x in empData:
-    print(x["name"])
-    
+def searchEmployee():
+    empData = loadEmployees()
+    inp = int(input("\nEnter Employee id: "))
+    for x in empData:
+        if x["id"] == inp:
+            print("Employee found!")
+            print(f"{x["name"]} has empolyee id {x["id"]}\n") 
+            return
+    print(f"Employee with {inp} not found!")
+
+def updateEmployee():
+
+
+
+while True:
+    x = int(input("\n0. Exit \n1. Search for Employee \n2. Add Employee \n3. Update Employee \n4. Print Employees \n\nEnter operation: "))
+
+    if x == 0:
+        break
+    elif x == 1:
+        searchEmployee()
+    elif x == 2:
+        updateEmployee()
+    elif x == 3:
+        addEmployee()
+    elif x == 4:
+        printEmployees()
+    else:
+        continue
+
+
+
+
+
+
+
+
+
+
+'''
+right now I have a list of objects - let's have real data of 15 json employees first
+empData is list of json objects
+
+update Employee
+    - load data
+    - modify specific chunck
+    - re-write into 
+
+'''
